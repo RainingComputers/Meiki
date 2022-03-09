4a7b7abaaccf337434f4118bf9a4e5e828e16336
alexsanjoseph <alexsanjoseph@gmail.com>
2022-03-10 02:01:23 +0530

    feat(UI): :sparkles: Added and styled items component
    with shnoo

--- meiki_ui/src/components/explorer/Item.svelte
+++ meiki_ui/src/components/explorer/Item.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
+    import ItemsIcon from "../icons/ItemsIcon.svelte"
     export let title: string
+
+    let isButtonChecked: boolean = false
+
+    function onClick() {
+        isButtonChecked = !isButtonChecked
+    }
+
+    export function isChecked() {
+        return isButtonChecked
+    }
 </script>
 
-<div>
-    {title}
+<div
+    class=" hover:bg-blue-500 hover:text-white text-slate-900 stroke-black hover:stroke-white "
+    on:click={onClick}
+>
+    <div
+        class="flex flex-row px-4 gap-3 py-1 items-center   "
+        class:bg-blue-600={isButtonChecked}
+        class:text-white={isButtonChecked}
+        class:stroke-white={isButtonChecked}
+    >
+        <span class=" h-4 w-4">
+            <ItemsIcon />
+        </span>
+        <span class=" font-light text-sm">{title}</span>
+    </div>
+    <!-- TODO: Fix styling  -->
 </div>
--- meiki_ui/src/components/explorer/Panel.svelte
+++ meiki_ui/src/components/explorer/Panel.svelte
@@ -2,6 +2,9 @@
     export let width: string = "24rem"
 </script>
 
-<div class=" bg-gray-50 border-gray-200 border" style="width: {width}">
+<div
+    class=" bg-gray-50 border-gray-200 border flex flex-col gap-1 py-2"
+    style="width: {width}"
+>
     <slot />
 </div>
