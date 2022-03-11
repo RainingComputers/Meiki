f4e2549e39cd5b48a97b7a6bf9c411d56138df11
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-03-12 01:03:13 +0530

    test(UI): :test_tube: Added a cypress test

---
+++ meiki_ui/cypress/integration/userFlow/userFlow.spec.js
@@ -0,0 +1,9 @@
+const baseUrl = "http://localhost:3000/login"
+
+describe("UserFlow Test", () => {
+    it("Goes to Create Page", () => {
+        cy.visit(baseUrl)
+        cy.get('img').should('have.attr', 'alt').should('include', 'meiki-logo')
+        cy.get("input").should("have.id", 'Username')
+    })
+})
