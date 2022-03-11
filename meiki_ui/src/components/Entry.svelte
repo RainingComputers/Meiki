ee560ecb60d02eda28018db905d262b7c693ee62
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-03-12 00:29:04 +0530

    fix(UI): :bug: Cleanup entry component
    with alex

--- meiki_ui/src/components/Entry.svelte
+++ meiki_ui/src/components/Entry.svelte
@@ -11,10 +11,11 @@
 </script>
 
 <div class=" flex flex-col w-full gap-1">
-    <span class=" text-sm">{label}</span>
+    <label for={label} class=" text-sm">{label}</label>
 
     <input
         bind:this={inputEl}
+        id={label}
         class=" bg-gray-200 focus:outline-none focus:border-blue-500 focus:ring-blue-500 focus:ring-2 rounded-lg p-2"
         {type}
         required
