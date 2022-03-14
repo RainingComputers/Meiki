5397510f89a5ac9b655994945e782aa8bc5889a8
alexsanjoseph <alexsanjoseph@gmail.com>
2022-03-15 01:58:01 +0530

    Added cypress json and linting fixes

--- meiki_ui/cypress/integration/userFlow/userFlow.spec.js
+++ meiki_ui/cypress/integration/userFlow/userFlow.spec.js
@@ -3,7 +3,7 @@ const baseUrl = "http://localhost:3000/login"
 describe("UserFlow Test", () => {
     it("Goes to Create Page", () => {
         cy.visit(baseUrl)
-        cy.get('img').should('have.attr', 'alt').should('include', 'meiki-logo')
-        cy.get("input").should("have.id", 'Username')
+        cy.get("img").should("have.attr", "alt").should("include", "meiki-logo")
+        cy.get("input").should("have.id", "Username")
     })
 })
