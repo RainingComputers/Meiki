cbd1e04fa0a5d2d1c4a21e4cd4a8a656522d3ac8
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-03-18 10:34:43 +0530

    style(UI): :pencil2: Fix duplicate class

--- meiki_ui/src/routes/index.svelte
+++ meiki_ui/src/routes/index.svelte
@@ -8,11 +8,11 @@
         class=" grid h-screen w-full v-screen font-serif text-gray-800 bg-gray-200 place-content-center dark:bg-gray-800 dark:text-gray-50 sm:p-0"
     >
         <h1 class="text-9xl">Meiki UI</h1>
-        <div class=" text-xl mt-8 text-sm">
+        <div class="mt-8 text-sm">
             New to Meiki notes? <Link text="Create" target="/create" /> a new account
             to get started
         </div>
-        <div class="text-xl mt-8 text-sm">
+        <div class="mt-8 text-sm">
             If you already have an account, Click <Link
                 text="here"
                 target="/login"
