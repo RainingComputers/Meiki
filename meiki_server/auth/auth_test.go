1996934a28410dad0f556fe1b7ef223307ed3bbb
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 00:48:03 +0530

    test(auth): :pencil2: Revert test context timeout

--- meiki_server/auth/auth_test.go
+++ meiki_server/auth/auth_test.go
@@ -32,7 +32,7 @@ func (s *AuthTestSuite) clean() {
 func (s *AuthTestSuite) SetupTest() {
 	log.Initialize()
 
-	s.ctx, s.cancel = context.WithTimeout(context.Background(), 500000000000*time.Millisecond) // 500ms might not be enough on some systems?
+	s.ctx, s.cancel = context.WithTimeout(context.Background(), 500*time.Millisecond) // 500ms might not be enough on some systems?
 
 	client, err := mongo.Connect(s.ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
 
