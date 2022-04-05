609a0dae376e715630e2110f33d7d5ccad9cd0aa
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 19:42:18 +0530

    refactor(UI): :recycle: Make button type an enum

--- meiki_ui/src/components/Button.svelte
+++ meiki_ui/src/components/Button.svelte
@@ -1,18 +1,31 @@
+<script context="module" lang="ts">
+    export enum ButtonType {
+        PRIMARY,
+        SECONDARY,
+    }
+
+    function getButtonClass(type: ButtonType) {
+        switch (type) {
+            case ButtonType.PRIMARY:
+                return "bg-blue-600 hover:bg-blue-500"
+            case ButtonType.SECONDARY:
+                return "bg-slate-600 hover:bg-slate-500"
+        }
+    }
+</script>
+
 <script lang="ts">
     export let label: string
     export let onClick: any = undefined
     export let fullWidth: boolean = false
-    export let type: string = "primary" //TODO: Make enum
+    export let type: ButtonType = ButtonType.PRIMARY
 
-    const classPrimary =
-        type == "primary" ? "bg-blue-600 hover:bg-blue-500" : ""
-    const classSecondary =
-        type == "secondary" ? "bg-slate-600 hover:bg-slate-500" : ""
+    const buttonClass = getButtonClass(type)
 </script>
 
 <button
     on:click={onClick}
-    class="drop-shadow-card font-bold text-white rounded-xl py-2 px-5 {classPrimary} {classSecondary}"
+    class="drop-shadow-card font-bold text-white rounded-xl py-2 px-5 {buttonClass}"
     class:w-full={fullWidth}
 >
     <span>{label}</span>
