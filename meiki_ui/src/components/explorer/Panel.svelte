2aba83129e5814b470a1bba2faebdc306731a367
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 21:21:51 +0530

    style(UI): :zap: Prevent transistion from making logout slow

--- meiki_ui/src/components/explorer/Panel.svelte
+++ meiki_ui/src/components/explorer/Panel.svelte
@@ -8,7 +8,7 @@
     class=" bg-gray-50 border-gray-200 border-r flex flex-col gap-1 py-5"
     style="width: {width}"
     data-cy="explorer"
-    transition:fly={{ x: -400, duration: 350, opacity: 1 }}
+    transition:fly|local={{ x: -400, duration: 350, opacity: 1 }}
 >
     <slot />
 </div>
