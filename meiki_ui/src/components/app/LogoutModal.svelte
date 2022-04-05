1f9d2eb3eb603bd6818b29c8b8ca5da8a0ed4ce4
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 22:36:13 +0530

    style(UI): :lipstick: Fix secondary button style and logout modal

--- meiki_ui/src/components/app/LogoutModal.svelte
+++ meiki_ui/src/components/app/LogoutModal.svelte
@@ -11,11 +11,11 @@
 </script>
 
 <div
-    class=" w-[600px] h-96 m-auto bg-gray-50 border-gray-200 rounded-xl flex flex-col py-0 px-6 gap-8"
+    class=" w-[500px] max-h-fit m-auto bg-gray-50 border-gray-200 rounded-xl flex flex-col py-5 px-6"
 >
     <div />
     <Logo />
-    <div class="px-5 flex flex-col">
+    <div class="px-4 py-5 flex flex-col">
         <Button
             label="Logout"
             type={ButtonType.SECONDARY}
@@ -23,7 +23,7 @@
         />
     </div>
 
-    <div class="flex flex-col text-center text-sm">
+    <div class="flex flex-col text-center text-sm mt-3">
         <div class="">The Meiki notes editor</div>
         <div class="">
             &#169; Copyright 2022 Vishnu Shankar B and Alex Joseph
