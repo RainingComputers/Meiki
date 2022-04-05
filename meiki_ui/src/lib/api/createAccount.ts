80d2a35fd02ec601ecddf22ec73382bda60beb20
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 20:03:20 +0530

    refactor(UI): :recycle: Make base URL common and optinally set from env variable

--- meiki_ui/src/lib/api/createAccount.ts
+++ meiki_ui/src/lib/api/createAccount.ts
@@ -1,12 +1,10 @@
 import { makeRequest } from "./request"
 
 export async function createAccount(username: string, password: string) {
-    const url = "http://localhost:8080/create" // TODO: Move this to env variables
-
     const body = {
         username,
         password,
     }
 
-    await makeRequest(url, "POST", body)
+    await makeRequest("/create", "POST", body)
 }
