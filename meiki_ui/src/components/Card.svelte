ac68d53f8c8e2b73260d977c858d28d0f92624c8
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 22:17:07 +0530

    style(UI): :zap: Add snappy transition for card component

--- meiki_ui/src/components/Card.svelte
+++ meiki_ui/src/components/Card.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+    import { scale } from "svelte/transition"
+
     export let width: number = 400
 </script>
 
 <span
     class=" bg-gray-50 border-gray-200 border drop-shadow-card "
     style="width: {width}px;"
+    transition:scale={{ duration: 30, opacity: 1 }}
 >
     <slot />
 </span>
