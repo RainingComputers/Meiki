37d99079d2b1a21823fc0046aeed218bc44d4532
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-05 22:02:47 +0530

    feat(UI): :zap: Make and add snappy transitions

--- meiki_ui/src/components/ModalOverlay.svelte
+++ meiki_ui/src/components/ModalOverlay.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { scale } from "svelte/transition"
+
     export let showOverlay: Boolean = false
 
     export function onClick() {
@@ -10,14 +12,20 @@
     }
 </script>
 
-<div
-    class="fixed inset-0 bg-gray-600 bg-opacity-60 h-full w-full flex flex-col align-center"
-    class:hidden={!showOverlay}
-    class:z-10={showOverlay}
-    data-cy="modalOverlay"
-    on:click={onClick}
->
-    <span on:click|stopPropagation class="max-w-fit m-auto">
-        <slot />
-    </span>
-</div>
+{#if showOverlay}
+    <div
+        class="fixed inset-0 bg-gray-600 bg-opacity-60 h-full w-full flex flex-col align-center"
+        class:hidden={!showOverlay}
+        class:z-10={showOverlay}
+        data-cy="modalOverlay"
+        on:click={onClick}
+    >
+        <span
+            on:click|stopPropagation
+            class="max-w-fit m-auto"
+            transition:scale={{ duration: 30, opacity: 1 }}
+        >
+            <slot />
+        </span>
+    </div>
+{/if}
