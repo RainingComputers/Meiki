01ac9201a9860962e7986ab94877149dc637d78b
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-06 22:31:49 +0530

    perf(UI): :zap: Make transitions feel snappier

--- meiki_ui/src/components/toast/Success.svelte
+++ meiki_ui/src/components/toast/Success.svelte
@@ -1,10 +1,12 @@
 <script>
     export let fullWidth = false
+    import { scale } from "svelte/transition"
 </script>
 
 <span
-    class=" text-green-500 font-bold bg-green-50 p-4 rounded-lg border-2 border-green-400 drop-shadow-card"
+    class=" text-green-500 bg-green-50 p-4 rounded-lg border-2 border-green-400 drop-shadow-card"
     class:w-full={fullWidth}
+    transition:scale={{ duration: 70, opacity: 1 }}
 >
     <slot />
 </span>
