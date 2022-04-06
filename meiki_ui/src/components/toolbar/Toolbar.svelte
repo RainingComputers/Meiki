f04f66b73ce5fe8c254e4bffd46df593b45490e4
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-06 22:31:49 +0530

    perf(UI): :zap: Make transitions feel snappier

--- meiki_ui/src/components/toolbar/Toolbar.svelte
+++ meiki_ui/src/components/toolbar/Toolbar.svelte
@@ -4,7 +4,7 @@
 
 <nav
     class="flex flex-row w-full h-12 drop-shadow-card bg-slate-800 items-center px-4 py-7 gap-2"
-    in:fly={{ y: -50, duration: 100, opacity: 1 }}
+    in:fly={{ y: -50, duration: 200, opacity: 1 }}
 >
     <slot />
 </nav>
