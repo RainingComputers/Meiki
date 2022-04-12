3620d9312cb3644f86689861c321fbca3095c2b2
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-13 00:54:19 +0530

    test(UI): :test_tube: Added tests for hero page flow

---
+++ meiki_ui/cypress/integration/heroFlow.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress"/>
+
+describe("HeroFlow Test", () => {
+    beforeEach(() => {
+        cy.testRequest("delete", false)
+        cy.clearLocalStorage()
+    })
+
+    it("Hero page goes to create page and login page", () => {
+        // shows hero page when not logged in
+        cy.visit("/")
+        cy.get("span:contains('lightning fast')")
+
+        // goes to create page on clicking create button
+        cy.get("button").click()
+        cy.get("Button").should("include.text", "Create Meiki account")
+
+        // goes to login page on clicking login link
+        cy.visit("/")
+        cy.get("a:contains('Click here')").click()
+        cy.get("Button").should("include.text", "Login")
+    })
+
+    it("Hero page redirects to app if logged in", () => {
+        cy.testRequest("create", true)
+        cy.testRequest("login", true)
+        cy.visit("/")
+
+        // goes to app if already logged in
+        cy.get("nav").should("be.visible")
+        cy.get("[data-cy='profile']").should("contain", "shnoo").click()
+    })
+})
