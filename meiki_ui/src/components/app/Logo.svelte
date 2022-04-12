310baf56d31bb036e3c8ca3d51ec9d62c2b27a98
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-13 00:24:32 +0530

    feat(UI): :sparkles: Added hero page

--- meiki_ui/src/components/app/Logo.svelte
+++ meiki_ui/src/components/app/Logo.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
     import logo from "$root/static/meiki.svg"
+
+    export let width: string = "100%"
 </script>
 
-<img src={logo} alt="meiki-logo" />
+<img src={logo} alt="meiki-logo" width={width} />
