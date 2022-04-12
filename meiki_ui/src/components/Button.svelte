60301fb6332e62cc1808c52342b7a8f0e3cffd3f
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-13 00:24:32 +0530

    feat(UI): :sparkles: Added hero page

--- meiki_ui/src/components/Button.svelte
+++ meiki_ui/src/components/Button.svelte
@@ -1,15 +1,18 @@
 <script context="module" lang="ts">
     export enum ButtonType {
+        HERO,
         PRIMARY,
         SECONDARY,
     }
 
     function getButtonClass(type: ButtonType) {
         switch (type) {
+            case ButtonType.HERO:
+                return "bg-blue-600 hover:bg-blue-500 text-white text-2xl py-4 px-10"
             case ButtonType.PRIMARY:
-                return "bg-blue-600 hover:bg-blue-500 text-white"
+                return "bg-blue-600 hover:bg-blue-500 text-white py-2 px-5"
             case ButtonType.SECONDARY:
-                return "bg-gray-300 hover:bg-gray-200 text-slate-700"
+                return "bg-gray-300 hover:bg-gray-200 text-slate-700 py-2 px-5"
         }
     }
 </script>
@@ -25,7 +28,7 @@
 
 <button
     on:click={onClick}
-    class="drop-shadow-card font-bold rounded-xl py-2 px-5 {buttonClass}"
+    class="drop-shadow-card font-bold rounded-xl {buttonClass}"
     class:w-full={fullWidth}
 >
     <span>{label}</span>
