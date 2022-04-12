245afb4c67c2920cfba4c79de0b20a9ddd72735f
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-04-13 00:34:57 +0530

    feat(UI): :sparkles: Added redirect to app if already logged in to hero page

--- meiki_ui/src/routes/index.svelte
+++ meiki_ui/src/routes/index.svelte
@@ -1,23 +1,39 @@
 <script lang="ts">
+    import { goto } from "$app/navigation"
+    import { onMount } from "svelte"
+    import { tokensPresent } from "$lib/api/request"
+
     import Root from "$cmp/Root.svelte"
     import Logo from "$cmp/app/Logo.svelte"
     import Button, { ButtonType } from "../components/Button.svelte"
     import Link from "../components/Link.svelte"
+
+    let loggedIn: boolean = true
+
+    onMount(() => {
+        loggedIn = tokensPresent()
+        if (loggedIn) goto("/notes")
+    })
 </script>
 
-<Root>
-    <span class="flex flex-col items-center gap-6">
-        <Logo width="50%" />
-        <h1 class="text-5xl">The ⚡ lightning fast ⚡ notes editor</h1>
-        <h1 class="text-2xl">
-            Meiki is a markdown notes editor built with performance and
-            simplicity in mind
-        </h1>
-    </span>
-    <span class="flex flex-col gap-4 p-14">
-        <Button type={ButtonType.HERO} label="Create Meiki Account" />
-        <span class="text-lg">
-            Already have an account? <Link text="Click here" target="/login" /> to login
+{#if !loggedIn}
+    <Root>
+        <span class="flex flex-col items-center gap-6">
+            <Logo width="50%" />
+            <h1 class="text-5xl">The ⚡ lightning fast ⚡ notes editor</h1>
+            <h1 class="text-2xl">
+                Meiki is a markdown notes editor built with performance and
+                simplicity in mind
+            </h1>
         </span>
-    </span>
-</Root>
+        <span class="flex flex-col gap-4 p-14">
+            <Button type={ButtonType.HERO} label="Create Meiki Account" />
+            <span class="text-lg">
+                Already have an account? <Link
+                    text="Click here"
+                    target="/login"
+                /> to login
+            </span>
+        </span>
+    </Root>
+{/if}
