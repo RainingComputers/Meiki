0bc67c606dc4117c8baded8248184f040e2b2ce6
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-01 14:42:29 +0530

    feat(UI): :sparkles: Added onEnter and cleaned up id in Entry component

--- meiki_ui/src/components/Entry.svelte
+++ meiki_ui/src/components/Entry.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
     export let label: string
     export let password: boolean = false
+    export let onEnter: any = undefined
 
     let inputEl: HTMLInputElement
     const type: string = password ? "password" : "text"
+    
+    const id: string = label.toLowerCase().replace(" ", "")
+
+    function onKeyDown(event: any) {
+        if (!onEnter) return
+        if (event.key !== "Enter" || event.target.value.length === 0) return
+        onEnter()
+    }
 
     export function getValue() {
         return inputEl.value
@@ -11,13 +20,14 @@
 </script>
 
 <div class=" flex flex-col w-full gap-1">
-    <label for={label} class=" text-sm">{label}</label>
+    <label for={id} class=" text-sm">{label}</label>
 
     <input
         bind:this={inputEl}
-        id={label}
+        id={id}
         class=" bg-gray-200 focus:outline-none focus:border-blue-500 focus:ring-blue-500 focus:ring-2 rounded-lg p-2"
         {type}
         required
+        on:keydown={onKeyDown}
     />
 </div>
