7c49bb773a930e19999539a01329958d04ac94cf
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-01 12:46:50 +0530

    refactor(UI): :recycle: Minor refactor of user.ts

--- meiki_ui/src/lib/api/user.ts
+++ meiki_ui/src/lib/api/user.ts
@@ -1,35 +1,29 @@
 import { makeRequest } from "./request"
 
-const AUTH_ROUTE_PREFIX = "auth"
-
 export async function createUser(username: string, password: string) {
     const body = { username, password }
-    await makeRequest(`${AUTH_ROUTE_PREFIX}/create`, "POST", body)
+    await makeRequest("auth/create", "POST", body)
 }
 
 export async function login(username: string, password: string) {
     const body = { username, password }
-    const response = await makeRequest(
-        `${AUTH_ROUTE_PREFIX}/login`,
-        "POST",
-        body
-    )
+    const response = await makeRequest("auth/login", "POST", body)
 
     localStorage.setItem("username", response.username)
     localStorage.setItem("token", response.token)
 }
 
 export async function authStatus() {
-    await makeRequest(`${AUTH_ROUTE_PREFIX}/authStatus`, "GET")
+    await makeRequest("auth/authStatus", "GET")
 }
 
 export async function deleteUser(username: string, password: string) {
     const body = { username, password }
-    await makeRequest(`${AUTH_ROUTE_PREFIX}/delete`, "POST", body)
+    await makeRequest("auth/delete", "POST", body)
 }
 
 export async function logout() {
-    await makeRequest(`${AUTH_ROUTE_PREFIX}/logout`, "POST")
+    await makeRequest("auth/logout", "POST")
     localStorage.clear()
 }
 
