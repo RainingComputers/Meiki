fddb1354251028d4bc3aba693fc2a53f69fe038b
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-14 16:03:29 +0530

    feat(UI): :sparkles: Added ace editor

--- meiki_ui/src/components/Editor.svelte
+++ meiki_ui/src/components/Editor.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
-    import currentNoteText from "$lib/stores/currentNoteText"
+    import { onDestroy, onMount } from "svelte"
+    // import currentNoteText from "$lib/stores/currentNoteText" // TODO: sync with current note text
+
+    export let fontSize: number
+    export let editorId: string = "testing" // TODO: change this
+
+    let editor: any = undefined
+
+    onMount(() => {
+        // @ts-ignore
+        window.ace.config.set("basePath", "/")
+        // @ts-ignore
+        editor = window.ace.edit(editorId)
+        editor.setTheme("ace/theme/xcode")
+        editor.session.setMode("ace/mode/markdown")
+        editor.setFontSize(fontSize)
+        editor.setShowPrintMargin(false)
+        editor.setHighlightActiveLine(false)
+    })
+
+    onDestroy(() => {
+        if (editor) editor.destroy()
+    })
+
+    export function focus() {
+        // set timeout so the editor does not capture key events like the enter key
+        // there is no other way to do this
+        setTimeout(() => editor.focus(), 1)
+    }
+
+    export function getValue(): string {
+        return editor.getValue()
+    }
+
+    export function setValue(content: string) {
+        editor.setValue(content, 1)
+    }
 </script>
 
-<textarea
-    class="w-full h-full resize-none p-2"
-    data-cy="editor"
-    bind:value={$currentNoteText}
-/>
+<div class="w-full h-full" id={editorId} />
--- meiki_ui/src/components/app/Workbench.svelte
+++ meiki_ui/src/components/app/Workbench.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+    import { afterUpdate } from "svelte"
     import Editor from "$cmp/Editor.svelte"
     import Renderer from "$cmp/Renderer.svelte"
     import Logo from "./Logo.svelte"
 
+    export let fontSize = 18
+
     let showRenderer = false
     let showEditor = false
+    let editor: Editor
+    let splitEditor: Editor
 
     export function toggleRenderer() {
         showRenderer = !showRenderer
@@ -13,12 +18,19 @@
     export function toggleEditor() {
         showEditor = !showEditor
     }
+
+    function focus() {
+        if (editor) editor.focus()
+        if (splitEditor) splitEditor.focus()
+    }
+
+    afterUpdate(focus)
 </script>
 
 <div class="flex flex-row flex-grow justify-center">
     {#if showEditor && !showRenderer}
-        <div class=" bg-green-100 w-3/5">
-            <Editor />
+        <div class=" bg-green-100 w-full">
+            <Editor {fontSize} bind:this={editor} />
         </div>
     {/if}
     {#if showRenderer && !showEditor}
@@ -27,12 +39,14 @@
         </div>
     {/if}
     {#if showRenderer && showEditor}
-        <div class=" bg-green-100 flex-1"><Editor /></div>
+        <div class=" bg-green-100 flex-1">
+            <Editor {fontSize} bind:this={splitEditor} />
+        </div>
         <div class=" bg-red-100 flex-1"><Renderer /></div>
     {/if}
     {#if !showRenderer && !showEditor}
         <div class="opacity-5 flex justify-center items-center">
-            <Logo width="70%" />
+            <Logo width="800em" />
         </div>
     {/if}
 </div>
