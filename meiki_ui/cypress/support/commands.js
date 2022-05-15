80492997ee9a8d1c6a4a24eaa1d628dca934f187
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-15 16:42:41 +0530

    refactor(UI): :recycle: Refactor test request command in cypress

--- meiki_ui/cypress/support/commands.js
+++ meiki_ui/cypress/support/commands.js
@@ -1,19 +1,23 @@
 const serverUrl = "http://localhost:8080"
 
-Cypress.Commands.add("testRequest", (method, command, failOnStatusCode) => {
-    const options = {
-        method: method,
-        url: `${serverUrl}${command}`,
-        failOnStatusCode: failOnStatusCode,
-        body: {
-            username: "shnoo",
-            password: "thisisveryunsafe",
-        },
-    }
+Cypress.Commands.add(
+    "testRequest",
+    (method, command, body, failOnStatusCode) => {
+        const options = {
+            method: method,
+            url: `${serverUrl}${command}`,
+            failOnStatusCode: failOnStatusCode,
+            body,
+            headers: {
+                "X-Username": localStorage.getItem("username"),
+                "X-Token": localStorage.getItem("token"),
+            },
+        }
 
-    const response = cy.request(options).then((response) => {
-        if (command !== "/auth/login") return
-        localStorage.setItem("username", response.body.username)
-        localStorage.setItem("token", response.body.token)
-    })
-})
+        cy.request(options).then((response) => {
+            if (command !== "/auth/login") return
+            localStorage.setItem("username", response.body.username)
+            localStorage.setItem("token", response.body.token)
+        })
+    }
+)
