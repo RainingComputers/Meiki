fd49ad260ca106ce6e73644656512d317b24b63e
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-25 02:00:36 +0530

    test(UI): :white_check_mark: Added tests for debounce logic and changed debouncer

--- meiki_ui/cypress/integration/syncAndSaveLogic.spec.js
+++ meiki_ui/cypress/integration/syncAndSaveLogic.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 
-const DEBOUNCE_INTERVAL = 5000
+const DEBOUNCE_INTERVAL = 500
 const EPSILON = 10
 
 describe("Note is saved and synchronized on changes", () => {
@@ -8,48 +8,41 @@ describe("Note is saved and synchronized on changes", () => {
         cy.login()
     })
 
-    // it("Note auto syncs", () => {
-    //     const testContent =
-    //         "This is a content to test if the note is synced and saved properly"
+    it("Note has a minimum sync interval", () => {
+        const testContent = "This is a content is not synced"
 
-    //     const keyboardSpeed = 100
-    //     const expectedNumChars = DEBOUNCE_INTERVAL / keyboardSpeed - EPSILON
+        cy.visit("/")
 
-    //     cy.visit("/")
+        cy.createNote("testNote")
+        cy.contains("testNote").click()
+        cy.get(".ace_text-input")
+            .first()
+            .focus()
+            .type(testContent, { delay: 70 })
 
-    //     cy.createNote("testNote")
-    //     cy.contains("testNote").click()
-    //     cy.get(".ace_text-input")
-    //         .first()
-    //         .focus()
-    //         .type(testContent, { delay: 100 })
+        cy.visit("/login")
+        cy.visit("/")
 
-    //     cy.visit("/login")
-    //     cy.visit("/")
+        cy.contains("testNote").click()
+        cy.get("[data-cy='renderer']").should("contain.text", "")
+    })
 
-    //     cy.contains("testNote").click()
-    //     cy.get("[data-cy='renderer']").should(
-    //         "contain.text",
-    //         testContent.slice(0, expectedNumChars - 1)
-    //     )
-    // })
+    it("Note saves on selected outside the note", () => {
+        const testContent =
+            "This is a content to test if the note is synced and saved properly"
 
-    // it("Note saves on selected outside the note", () => {
-    //     const testContent =
-    //         "This is a content to test if the note is synced and saved properly"
+        cy.visit("/")
 
-    //     cy.visit("/")
+        cy.createNote("testNote")
+        cy.contains("testNote").click()
+        cy.get(".ace_text-input").first().focus().type(testContent)
 
-    //     cy.createNote("testNote")
-    //     cy.contains("testNote").click()
-    //     cy.get(".ace_text-input").first().focus().type(testContent)
+        cy.contains("[data-cy='explorer']", "testNote").click()
+        cy.visit("/")
 
-    //     cy.contains("[data-cy='explorer']", "testNote").click()
-    //     cy.visit("/")
-
-    //     cy.contains("testNote").click()
-    //     cy.get("[data-cy='renderer']").should("contain.text", testContent)
-    // })
+        cy.contains("testNote").click()
+        cy.get("[data-cy='renderer']").should("contain.text", testContent)
+    })
 
     it("Note should be saved if the user waited for enough time", () => {
         const testContent =
@@ -59,11 +52,8 @@ describe("Note is saved and synchronized on changes", () => {
 
         cy.createNote("testNote")
         cy.contains("testNote").click()
-        cy.get(".ace_text-input")
-            .first()
-            .focus()
-            .type(testContent, { delay: 75 })
-        cy.wait(DEBOUNCE_INTERVAL + EPSILON)
+        cy.get(".ace_text-input").first().focus().type(testContent)
+        cy.wait(DEBOUNCE_INTERVAL)
 
         cy.visit("/login")
         cy.visit("/")
