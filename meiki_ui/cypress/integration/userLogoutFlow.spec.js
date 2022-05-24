3d32e45cf0c6de608840813a6a8173d83b7d0d65
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-25 00:12:13 +0530

    test(UI): :white_check_mark: Added test for logout flow

--- meiki_ui/cypress/integration/userLogoutFlow.spec.js
+++ meiki_ui/cypress/integration/userLogoutFlow.spec.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress"/>
 
 describe("User logout", () => {
+    beforeEach(() => {
+        cy.login()
+    })
+
     it("User can logout", () => {
-        /* TODO */
+        cy.visit("/")
+
+        cy.get("[data-cy='profile']").click()
+        cy.contains("Logout").click()
+        cy.get("Button").should("include.text", "Login").and("be.visible")
     })
 
     it("Errors out with unable to connect to server", () => {
