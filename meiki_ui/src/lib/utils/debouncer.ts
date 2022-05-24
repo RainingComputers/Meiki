9df581f7014956f5bdf8fd84baeeff21f954cd3a
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-25 02:00:36 +0530

    test(UI): :white_check_mark: Added tests for debounce logic and changed debouncer

--- meiki_ui/src/lib/utils/debouncer.ts
+++ meiki_ui/src/lib/utils/debouncer.ts
@@ -1,18 +1,46 @@
 type VoidReturnAsyncFunc = (...args: any[]) => Promise<void>
 
+// Based on https://github.com/component/debounce/blob/master/index.js
+
 export function debounce(
     func: VoidReturnAsyncFunc,
-    threshold: number = 5000
+    wait: number = 500,
+    immediate: boolean = false
 ): VoidReturnAsyncFunc {
-    let lastCall = new Date()
+    var timeout: NodeJS.Timeout,
+        args: any,
+        context: any,
+        timestamp: number,
+        result: Promise<void>
+    if (null == wait) wait = 100
 
-    return async function (...args: any[]) {
-        const now = new Date()
-        const delta = now.getTime() - lastCall.getTime()
+    function later() {
+        var last = Date.now() - timestamp
 
-        if (delta > threshold) {
-            await func(...args)
-            lastCall = now
+        if (last < wait && last >= 0) {
+            timeout = setTimeout(later, wait - last)
+        } else {
+            timeout = null
+            if (!immediate) {
+                result = func.apply(context, args)
+                context = args = null
+            }
         }
     }
+
+    var debounced = async function () {
+        context = this
+        args = arguments
+        timestamp = Date.now()
+        var callNow = immediate && !timeout
+        if (!timeout) timeout = setTimeout(later, wait)
+        if (callNow) {
+            result = await func.apply(context, args)
+            context = args = null
+        }
+
+        return result
+    }
+
+    return debounced
 }
