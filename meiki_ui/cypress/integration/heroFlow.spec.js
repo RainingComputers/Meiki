db7a913bce0c10746c513dbbb0331bc8883e0fe3
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-05-25 18:08:46 +0530

    style(UI): :art: Standardise non TODO comments

--- meiki_ui/cypress/integration/heroFlow.spec.js
+++ meiki_ui/cypress/integration/heroFlow.spec.js
@@ -6,15 +6,15 @@ describe("Hero page", () => {
     })
 
     it("Hero page goes to create page and login page", () => {
-        // shows hero page when not logged in
+        // Shows hero page when not logged in
         cy.visit("/")
         cy.get("span:contains('lightning fast')")
 
-        // goes to create page on clicking create button
+        // Goes to create page on clicking create button
         cy.get("button").click()
         cy.get("Button").should("include.text", "Create Meiki account")
 
-        // goes to login page on clicking login link
+        // Goes to login page on clicking login link
         cy.visit("/")
         cy.get("a:contains('Click here')").click()
         cy.get("Button").should("include.text", "Login")
@@ -24,7 +24,7 @@ describe("Hero page", () => {
         cy.login()
         cy.visit("/")
 
-        // goes to app if already logged in
+        // Goes to app if already logged in
         cy.get("nav").should("be.visible")
         cy.get("[data-cy='profile']").should("contain", "shnoo").click()
     })
--- meiki_ui/cypress/integration/workbench.spec.js
+++ meiki_ui/cypress/integration/workbench.spec.js
@@ -15,47 +15,47 @@ describe("Workbench", () => {
     it("Workbench shows rendered and editor accordingly", () => {
         cy.visit("/notes")
 
-        // both should not be visible when app has been opened
+        // Both should not be visible when app has been opened
         cy.get("[data-cy='editor']").should("not.exist")
         cy.get("[data-cy='renderer']").should("not.exist")
 
-        // create note
+        // Create note
         cy.createNote("testNote")
 
-        // select a note
+        // Select a note
         cy.contains("testNote").click()
 
-        // both editor and render should be visible
+        // Both editor and render should be visible
         cy.get("[data-cy='editor']").should("be.visible")
         cy.get("[data-cy='renderer']").should("be.visible")
 
-        // close editor
+        // Close editor
         cy.get("[data-cy='edit']").should("have.class", "isChecked").click()
 
-        // editor should not be visible but the renderer should be visible
+        // Editor should not be visible but the renderer should be visible
         cy.get("[data-cy='editor']").should("not.be.visible")
         cy.get("[data-cy='renderer']").should("be.visible")
 
-        // open editor again
+        // Open editor again
         cy.get("[data-cy='edit']").should("not.have.class", "isChecked").click()
 
-        // both editor and renderer should be visible
+        // Both editor and renderer should be visible
         cy.get("[data-cy='renderer']").should("be.visible")
         cy.get("[data-cy='editor']").should("be.visible")
 
-        // close renderer
+        // Close renderer
         cy.get("[data-cy='render']").should("have.class", "isChecked").click()
 
-        // editor should be visible but the renderer should not be visible
+        // Editor should be visible but the renderer should not be visible
         cy.get("[data-cy='renderer']").should("not.be.visible")
         cy.get("[data-cy='editor']").should("be.visible")
 
-        // open renderer again
+        // Open renderer again
         cy.get("[data-cy='render']")
             .should("not.have.class", "isChecked")
             .click()
 
-        // both editor and renderer should be visible
+        // Both editor and renderer should be visible
         cy.get("[data-cy='renderer']").should("be.visible")
         cy.get("[data-cy='editor']").should("be.visible")
     })
