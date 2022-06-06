186bcb6e74d9049b1c1f971052b362661346e4db
alexsanjoseph <alexsanjoseph@gmail.com>
2022-06-07 00:58:47 +0530

    feat(UI): :sparkles: Markdoc for rendering markdown in the renderer
    with shnoo

--- meiki_ui/src/components/Renderer.svelte
+++ meiki_ui/src/components/Renderer.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+    import Markdoc from "@markdoc/markdoc"
+
     export let text: string = ""
+
+    $: ast = Markdoc.parse(text)
+    $: content = Markdoc.transform(ast)
 </script>
 
-<pre data-cy="renderer" class="p-2 overflow-scroll h-full">{text}</pre>
+<p data-cy="renderer" class="p-2 overflow-scroll h-full prose">
+    {@html Markdoc.renderers.html(content)}
+</p>
