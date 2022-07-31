24db57556b613f1f9c1f417b4d08a511742596c5
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-07-31 17:38:39 +0530

    feat(UI): :sparkles: Added white outline for logo in dark theme

--- meiki_ui/src/components/App.svelte
+++ meiki_ui/src/components/App.svelte
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col m-0 absolute items-center h-screen place-contentTop w-full overflow-clip bg-base-0"
+    class="flex flex-col m-0 absolute items-center h-screen place-contentTop w-full overflow-clip bg-base-0 stroke-content"
 >
     <slot />
 </div>
--- meiki_ui/src/components/Root.svelte
+++ meiki_ui/src/components/Root.svelte
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col m-0 absolute items-center justify-center h-screen place-contentCenter w-full bg-base-0 text-content"
+    class="flex flex-col m-0 absolute items-center justify-center h-screen place-contentCenter w-full bg-base-0 text-content stroke-content"
 >
     <slot />
 </div>
