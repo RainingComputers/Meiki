3854fd948797834546993a679e81d25cf6fff2c0
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-07-31 17:56:31 +0530

    style(UI): :lipstick: Change editor theme

--- meiki_ui/src/global.d.ts
+++ meiki_ui/src/global.d.ts
@@ -5,6 +5,7 @@ declare module "brace/mode/markdown"
 declare module "brace/theme/textmate"
 declare module "brace/theme/monokai"
 declare module "brace/theme/tomorrow_night_bright"
+declare module "brace/theme/twilight"
 
 interface ImportMetaEnv {
     readonly VITE_MEIKI_SERVER_URL: string
