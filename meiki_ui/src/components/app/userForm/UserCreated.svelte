7548a8197bef9ea73ce32221bea6fe07f89d8e0e
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-07-31 16:43:44 +0530

    refactor(UI): :recycle: Remove theming from app components and routes

--- meiki_ui/src/components/app/userForm/UserCreated.svelte
+++ meiki_ui/src/components/app/userForm/UserCreated.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <Root>
-    <div class="flex flex-col items-center gap-5 text-content">
+    <div class="flex flex-col items-center gap-5">
         <Success>Your account has successfully been created</Success>
         <span>
             Click <Link target="/login" text="here" /> to login
