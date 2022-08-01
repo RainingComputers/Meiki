3561012b42a1c42527092396ec5808cdb6b35872
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-08-02 00:37:32 +0530

    style(UI): :lipstick: Change searchbar border width

--- meiki_ui/src/components/Searchbar.svelte
+++ meiki_ui/src/components/Searchbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="flex flex-row border-2 bg-base-0 border-base-2 focus-within:border-primary-focus rounded-xl p-2 flex-grow"
+    class="flex flex-row outline outline-1 focus-within:outline-2 outline-base-2 bg-base-0 focus-within:outline-primary-focus rounded-xl p-2 flex-grow"
 >
     <input
         class=" bg-base-0 placeholder-placeholder outline-none border-0 flex-grow text-content"
