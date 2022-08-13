d7d1c9c3ce3d5e37c1a4655331de26942650b9ee
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-08-13 15:07:44 +0530

    style(UI): :lipstick: Fix confirmation modal dark theme

--- meiki_ui/src/components/modal/Window.svelte
+++ meiki_ui/src/components/modal/Window.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="w-96 max-h-fit m-auto bg-base-1 border-base-2 rounded-xl flex flex-col py-6 px-6"
+    class="w-96 max-h-fit m-auto bg-base-1 border-base-2 rounded-xl flex flex-col py-6 px-6 text-content"
     style="width: {width};"
 >
     <slot />
